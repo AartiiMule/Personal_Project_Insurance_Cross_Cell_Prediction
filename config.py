5ee9bb4fa3eaf7155ba29da30c3b392f3709679a
AartiiMule <muleaarti0505@gmail.com>
2024-03-19 20:37:19 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+csv_file_path = "file.csv"
+
+port_number = 5005
+
+MODEL_FILE_PATH = os.path.join("project_app", "adaboost_model.pkl")
+
+JSON_FILE_PATH = os.path.join("project_app", "json_file.json")
